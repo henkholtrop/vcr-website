d17343a7a9b1bf6f6f82fde66c2310f96b5c264d
Peter Alserda <peteralserda@hotmail.com>
2015-03-27 10:47:15 +0100

    merge conflict fixed

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,8 @@
 class Team < ActiveRecord::Base
-  has_and_belongs_to_many :games, dependent: :destroy
+  belongs_to :game
   validates :title, presence: true,
                     length: { minimum: 2 }
 
   has_attached_file :logo
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
-<<<<<<< HEAD
-=======
-  belongs_to :game
->>>>>>> f1d7283e082e106d400bc3b3e8ad96bdca219673
 end
