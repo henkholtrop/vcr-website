af8e4318454b23c65b790d05aa38c780678f854e
Henk Holtrop <henk_holtrop@live.nl>
2015-06-12 15:08:01 +0200

    I dont like bootsy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611072641) do
+ActiveRecord::Schema.define(version: 20150612122621) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
